a2b552ab04ab3c81e662bc76925b6201d6775f28
Angus.Young <angusyoung@qq.com>
2016-12-10 18:53:28 +0800

    Update
    * 重构了选项页和POP页

--- js/options.js
+++ js/options.js
@@ -4,23 +4,59 @@
  * @Since 16/2/18
  */
 
+var $ = jClass;
 $(function () {
-	var wLocalSto = window.localStorage;
-	var $Url = $('#url');
-	var $CatchUrl = $('#catchUrl');
-	var $TargetUrl = $('#targetUrl');
+	var oLocalStorage = window.localStorage;
+	var sType = oLocalStorage.getItem('catch.opt.type');
+	var sRule = oLocalStorage.getItem('catch.opt.rule');
+	var sSync = oLocalStorage.getItem('catch.opt.sync');
+	var sResult = oLocalStorage.getItem('catch.opt.result');
+	var sDate = oLocalStorage.getItem('catch.opt.syncDate');
 
-	$Url.val(wLocalSto.getItem('CC_postUrl') || '');
-	$CatchUrl.val(wLocalSto.getItem('CC_catchUrl') || '');
-	$TargetUrl.val(wLocalSto.getItem('CC_targetUrl') || '');
+	$('.tab-opt').hide();
+	$('#' + sType).show();
+	$('@type[value="' + sType + '"]').attr('checked', true);
+	$('#rule').val(sRule);
+	$('#syncUrl').val(sSync);
+	$('#syncDate').html(sDate);
+	$('#syncResult').val(sResult);
 
-	$('#save').on('click', function () {
-		wLocalSto.setItem('CC_postUrl', $Url.val());
-		wLocalSto.setItem('CC_catchUrl', $CatchUrl.val());
-		wLocalSto.setItem('CC_targetUrl', $TargetUrl.val());
-		$(this).addClass('button-success').text('保存成功');
+	$('form input[type="radio"]').on('change', function () {
+		$('.tab-opt').hide();
+		$('#' + this.value).show();
 	});
-	$('input').on('keypress', function () {
-		$('#save').removeClass('button-success').text('保存');
+
+	$('#getSync').on('click', function () {
+		var sURL = $('#syncUrl').val();
+		var oIcon = $('i', this);
+		oLocalStorage.setItem('catch.opt.sync', sURL);
+		oIcon.addClass('sa-icon-spin');
+		$.ajax({
+			method : 'get',
+			url    : sURL,
+			success: function (jRes) {
+				var dDate = new Date();
+				$('#syncDate').html(dDate.toLocaleString());
+				$('#syncResult').val(jRes);
+				oLocalStorage.setItem('catch.opt.syncDate', dDate.toLocaleString());
+				oIcon.removeClass('sa-icon-spin');
+			},
+			error  : function () {
+				alert('同步失败，请稍候重试！');
+				oIcon.removeClass('sa-icon-spin');
+			}
+		});
 	});
+
+	$('form').on('submit', function () {
+		var oElement = this.elements;
+
+		oLocalStorage.setItem('catch.opt.type', oElement['type'].value);
+		oLocalStorage.setItem('catch.opt.rule', oElement['rule'].value);
+		oLocalStorage.setItem('catch.opt.result', oElement['syncResult'].value);
+
+		alert('保存成功！');
+		return false;
+	});
+
 });
--- js/popup.js
+++ js/popup.js
@@ -4,71 +4,99 @@
  * @Since 16/2/3
  */
 
+var $ = jClass;
 $(function () {
 	var bg = chrome.extension.getBackgroundPage();
-	chrome.tabs.query({active: true, currentWindow: true}, function (aTab) {
-		var nTabId = aTab[0].id;
-		var wLocalSto = window.localStorage;
+	var oLocalStorage = window.localStorage;
 
-		var $Total = $('#total');
-		var $StartId = $('#startId');
-		var $Delay = $('#delay');
-		var $Loop = $('#loop');
-		var $Retry = $('#retry');
+	var $RemoteUrl = $('#remoteUrl');
+	var $SubmitUrl = $('#submitUrl');
+	var $RetryTime = $('#retryTime');
 
-		$StartId.val(wLocalSto.getItem('CC_startId'));
-		$Total.val(wLocalSto.getItem('CC_total'));
-		$Delay.val(wLocalSto.getItem('CC_delay'));
-		$Loop.val(wLocalSto.getItem('CC_loop'));
-		$Retry.val(wLocalSto.getItem('CC_retry'));
+	function fCheckRequire() {
+		if ($RemoteUrl.val()) {
+			$('#takeTask').removeAttr('disabled');
+		}
+		else {
+			$('#takeTask').attr('disabled', true);
+		}
 
-		$StartId.on('change', function () {
-			wLocalSto.setItem('CC_startId', $(this).val());
-			$(this).removeClass('has-error');
-		});
-		$Total.on('change', function () {
-			wLocalSto.setItem('CC_total', $(this).val());
-			$(this).removeClass('has-error');
-		});
-		$Delay.on('change', function () {
-			wLocalSto.setItem('CC_delay', $(this).val());
-			$(this).removeClass('has-error');
-		});
-		$Loop.on('change', function () {
-			wLocalSto.setItem('CC_loop', $(this).val());
-			$(this).removeClass('has-error');
-		});
-		$Retry.on('change', function () {
-			wLocalSto.setItem('CC_retry', $(this).val());
-			$(this).removeClass('has-error');
-		});
+		if ($SubmitUrl.val()) {
+			$('#start').removeAttr('disabled');
+		}
+		else {
+			$('#start').attr('disabled', true);
+		}
+	}
 
-		$('#start').on('click', function () {
-			var bDone = true;
-			$('form input').each(function () {
-				if (!this.value.toString().length) {
-					$(this).addClass('has-error');
-					bDone = false;
-					return false;
+	$RemoteUrl.val(oLocalStorage.getItem('catch.set.remote'));
+	$SubmitUrl.val(oLocalStorage.getItem('catch.set.submit'));
+	$RetryTime.val(oLocalStorage.getItem('catch.set.retry'));
+
+	fCheckRequire();
+
+	//	显示已存在的任务
+	var sTasks = bg.fGetTask();
+	if (sTasks.length) {
+		$('#taskInfo').removeClass('hidden').find('span').html(sTasks.split(',').length);
+	}
+
+	$RemoteUrl.on('change', function () {
+		oLocalStorage.setItem('catch.set.remote', $(this).val());
+		fCheckRequire();
+	});
+	$SubmitUrl.on('change', function () {
+		oLocalStorage.setItem('catch.set.submit', $(this).val());
+		if (this.value === '') {
+			$(this).addClass('border-error');
+		}
+		else {
+			$(this).removeClass('border-error');
+		}
+		fCheckRequire();
+	});
+	$RetryTime.on('change', function () {
+		oLocalStorage.setItem('catch.set.retry', $(this).val());
+	});
+
+	// 接收任务
+	$('#takeTask').on('click', function () {
+		$.ajax({
+			url     : $RemoteUrl.val(),
+			cache   : false,
+			dataType: 'json',
+			success : function (jRes) {
+				if (jRes.error) {
+					jRes = JSON.parse(jRes.source);
 				}
-			});
-
-			if (bDone) {
-				wLocalSto.setItem('CC_startId', $StartId.val());
-				wLocalSto.setItem('CC_total', $Total.val());
-				wLocalSto.setItem('CC_delay', $Delay.val());
-				wLocalSto.setItem('CC_loop', $Loop.val());
-				wLocalSto.setItem('CC_retry', $Retry.val());
-
-				bg.fStart(nTabId);
-				window.close();
+				bg.fSetTask(jRes);
+				$('#taskInfo').removeClass('hidden').find('span').html(jRes.length);
+				$(this).addClass('btn-success');
+			},
+			error   : function () {
+				$(this).addClass('btn-error');
 			}
 		});
+	});
 
-		$('#catch').on('click', function () {
-			bg.fInit(nTabId);
-			bg.fCatch(true);
+	// 获取当前激活窗口标签的信息
+	chrome.tabs.query({active: true, currentWindow: true}, function (aTab) {
+		var nTabId = aTab[0].id;
+		var sUrl = aTab[0].url;
+
+		// 执行任务
+		$('@setting').on('submit', function () {
+			var sTask = bg.fGetTask();
+			sTask = sTask || sUrl;
+			var aTask = sTask.split(',');
+
+			oLocalStorage.setItem('catch.set.remote', $RemoteUrl.val());
+			oLocalStorage.setItem('catch.set.submit', $SubmitUrl.val());
+			oLocalStorage.setItem('catch.set.retry', $RetryTime.val());
+
+			bg.fStart(nTabId, aTask);
 			window.close();
+			return false;
 		});
 	});
 });
